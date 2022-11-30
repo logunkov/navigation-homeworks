c8e88504bba01f9cc4a774de3fe53b6e9c93d9ef
logunkov <logunkovkn@gmail.com>
2022-11-30 17:16:34 +0700

    Fix InfoViewController.swift

--- Navigation/InfoViewController.swift
+++ Navigation/InfoViewController.swift
@@ -21,14 +21,22 @@ class InfoViewController: UIViewController {
         return button
     }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = .yellow
-        view.addSubview(self.button)
+    private func installButton(button: UIButton){
         self.button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
         self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
         self.button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
         self.button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+        view.addSubview(self.button)
+        installButton(button: button)
+//        self.button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
+//        self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+//        self.button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
+//        self.button.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
 
     @objc private func buttonAction() {
