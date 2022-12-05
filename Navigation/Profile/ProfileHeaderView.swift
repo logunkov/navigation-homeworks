66f7e1c447cec6cd633df46151713494e4a23f81
logunkov <logunkovkn@gmail.com>
2022-12-05 13:42:22 +0700

    Fix Profile/ProfileHeaderView.swift

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -12,28 +12,33 @@ class ProfileHeaderView: UIView {
     private var statusText: String?
 
     var labelName: UILabel = {
-        let labelName = UILabel(frame: CGRect(x: 170, y: 90 + 27, width: 204, height: 18))
+//        let labelName = UILabel(frame: CGRect(x: 170, y: 90 + 27, width: 204, height: 18))
+        let labelName = UILabel()
         //labelName.backgroundColor = .yellow
         labelName.text = "Popeye the Sailor"
         labelName.font = UIFont.boldSystemFont(ofSize: 18)
         labelName.textColor = .black
         labelName.textAlignment = .center
+        labelName.translatesAutoresizingMaskIntoConstraints = false
         return labelName
     }()
 
     var labelStatus: UILabel = {
-        let labelStatus = UILabel(frame: CGRect(x: 170, y: 256 + 16 - 34 - 14, width: 204, height: 14))
+//        let labelStatus = UILabel(frame: CGRect(x: 170, y: 256 + 16 - 34 - 14, width: 204, height: 14))
+        let labelStatus = UILabel()
         //labelStatus.backgroundColor = .yellow
         labelStatus.text = "Шпинат - Сила!"
         labelStatus.numberOfLines = 0
         labelStatus.font = UIFont.boldSystemFont(ofSize: 14)
         labelStatus.textColor = .gray
         labelStatus.textAlignment = .center
+        labelStatus.translatesAutoresizingMaskIntoConstraints = false
         return labelStatus
     }()
 
     var button: UIButton = {
-        let button = UIButton(frame: CGRect(x: 16, y: 256 + 16 + 20, width: 374 - 16, height: 50))
+//        let button = UIButton(frame: CGRect(x: 16, y: 256 + 16 + 20, width: 374 - 16, height: 50))
+        let button = UIButton()
         button.backgroundColor = .systemBlue
         button.setTitle("Set status", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
@@ -44,23 +49,27 @@ class ProfileHeaderView: UIView {
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOpacity = 0.7
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
     var imageView: UIImageView = {
         let size = 150
-        let imageView = UIImageView(frame: CGRect(x: 16, y: 90 + 16, width: size, height: size))
+//        let imageView = UIImageView(frame: CGRect(x: 16, y: 90 + 16, width: size, height: size))
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: size, height: size))
         imageView.image = UIImage(named: "popeye")
         imageView.layer.borderWidth = 3
         imageView.layer.backgroundColor = UIColor.purple.cgColor
         imageView.layer.borderColor = UIColor.white.cgColor
         imageView.layer.cornerRadius = imageView.frame.size.width / 2
         imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
 
     var textField: UITextField = {
-        let textField = UITextField(frame: CGRect(x: 170, y: 256 - 12, width: 204, height: 40))
+//        let textField = UITextField(frame: CGRect(x: 170, y: 256 - 12, width: 204, height: 40))
+        let textField = UITextField()
         textField.text = ""
         textField.font = UIFont.systemFont(ofSize: 15)
         textField.textColor = .black
@@ -70,17 +79,20 @@ class ProfileHeaderView: UIView {
         textField.layer.borderColor = UIColor.black.cgColor
         textField.textAlignment = .center
         textField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
 
     init() {
-        super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        super.init(frame: .zero)
 
         self.addSubview(labelName)
         self.addSubview(labelStatus)
         self.addSubview(imageView)
         self.addSubview(button)
         self.addSubview(textField)
+
+        installConstrains()
     }
 
     required init?(coder: NSCoder) {
@@ -96,5 +108,32 @@ class ProfileHeaderView: UIView {
         if let text = textField.text {
             statusText = text
         }
+    }
+
+    private func installConstrains() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            imageView.widthAnchor.constraint(equalToConstant: 150),
+            imageView.heightAnchor.constraint(equalToConstant: 150),
+
+            button.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 16),
+            button.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            button.heightAnchor.constraint(equalToConstant: 50),
+
+            labelName.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
+            labelName.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 8),
+            labelName.trailingAnchor.constraint(equalTo: button.trailingAnchor),
+
+            textField.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 8),
+            textField.trailingAnchor.constraint(equalTo: button.trailingAnchor),
+            textField.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -8),
+            textField.heightAnchor.constraint(equalToConstant: 40),
+
+            labelStatus.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 8),
+            labelStatus.trailingAnchor.constraint(equalTo: button.trailingAnchor),
+            labelStatus.bottomAnchor.constraint(equalTo: textField.topAnchor, constant: -8)
+        ])
     }
 }
