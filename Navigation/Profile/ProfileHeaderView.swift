9cf746c56108a003ba2263f9459ea8023236bdc5
logunkov <logunkovkn@gmail.com>
2022-12-05 10:53:08 +0700

    Fix ProfileHeaderView.swift

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -11,7 +11,7 @@ class ProfileHeaderView: UIView {
 
     private var statusText: String?
 
-    var labelName: UILabel {
+    var labelName: UILabel = {
         let labelName = UILabel(frame: CGRect(x: 170, y: 90 + 27, width: 204, height: 18))
         //labelName.backgroundColor = .yellow
         labelName.text = "Popeye the Sailor"
@@ -19,22 +19,20 @@ class ProfileHeaderView: UIView {
         labelName.textColor = .black
         labelName.textAlignment = .center
         return labelName
-    }
+    }()
 
-    var labelStatus: UILabel
-// Было
-//    {
-//        let labelStatus = UILabel(frame: CGRect(x: 170, y: 256 + 16 - 34 - 14, width: 204, height: 14))
-//        //labelStatus.backgroundColor = .yellow
-//        labelStatus.text = "Шпинат - Сила!"
-//        labelStatus.numberOfLines = 0
-//        labelStatus.font = UIFont.boldSystemFont(ofSize: 14)
-//        labelStatus.textColor = .gray
-//        labelStatus.textAlignment = .center
-//        return labelStatus
-//    }
+    var labelStatus: UILabel = {
+        let labelStatus = UILabel(frame: CGRect(x: 170, y: 256 + 16 - 34 - 14, width: 204, height: 14))
+        //labelStatus.backgroundColor = .yellow
+        labelStatus.text = "Шпинат - Сила!"
+        labelStatus.numberOfLines = 0
+        labelStatus.font = UIFont.boldSystemFont(ofSize: 14)
+        labelStatus.textColor = .gray
+        labelStatus.textAlignment = .center
+        return labelStatus
+    }()
 
-    var button: UIButton {
+    var button: UIButton = {
         let button = UIButton(frame: CGRect(x: 16, y: 256 + 16 + 20, width: 374 - 16, height: 50))
         button.backgroundColor = .systemBlue
         button.setTitle("Set status", for: .normal)
@@ -47,9 +45,9 @@ class ProfileHeaderView: UIView {
         button.layer.shadowOpacity = 0.7
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         return button
-    }
+    }()
 
-    var imageView: UIImageView {
+    var imageView: UIImageView = {
         let size = 150
         let imageView = UIImageView(frame: CGRect(x: 16, y: 90 + 16, width: size, height: size))
         imageView.image = UIImage(named: "popeye")
@@ -58,11 +56,10 @@ class ProfileHeaderView: UIView {
         imageView.layer.borderColor = UIColor.white.cgColor
         imageView.layer.cornerRadius = imageView.frame.size.width / 2
         imageView.clipsToBounds = true
-
         return imageView
-    }
+    }()
 
-    var textField: UITextField {
+    var textField: UITextField = {
         let textField = UITextField(frame: CGRect(x: 170, y: 256 - 12, width: 204, height: 40))
         textField.text = ""
         textField.font = UIFont.systemFont(ofSize: 15)
@@ -73,15 +70,12 @@ class ProfileHeaderView: UIView {
         textField.layer.borderColor = UIColor.black.cgColor
         textField.textAlignment = .center
         textField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
-
         return textField
-    }
+    }()
 
     init() {
-        // Стало
-        self.labelStatus = UILabel(frame: CGRect(x: 170, y: 256 + 16 - 34 - 14, width: 204, height: 14))
         super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        creatingLabelStatus()
+
         self.addSubview(labelName)
         self.addSubview(labelStatus)
         self.addSubview(imageView)
@@ -102,15 +96,5 @@ class ProfileHeaderView: UIView {
         if let text = textField.text {
             statusText = text
         }
-    }
-
-    func creatingLabelStatus() {
-        // Стало
-        //labelStatus.backgroundColor = .yellow
-        labelStatus.text = "Шпинат - Сила!"
-        labelStatus.numberOfLines = 0
-        labelStatus.font = UIFont.boldSystemFont(ofSize: 14)
-        labelStatus.textColor = .gray
-        labelStatus.textAlignment = .center
     }
 }
