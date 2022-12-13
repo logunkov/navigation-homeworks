f56b38c3b8ab9df6d5671390c08fded29068f27f
logunkov <logunkovkn@gmail.com>
2022-12-13 21:01:54 +0700

    Fix News/Post.swift

--- Navigation/News/Post.swift
+++ Navigation/News/Post.swift
@@ -8,5 +8,10 @@
 import UIKit
 
 struct Post {
-    var title: String
+//    var title: String
+    let author: String
+    let description: String
+    let image: String
+    let likes: Int
+    let views: Int
 }
