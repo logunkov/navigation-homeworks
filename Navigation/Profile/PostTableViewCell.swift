1fd019933af683f2f6e5767e2e04a6558d0f6c76
logunkov <logunkovkn@gmail.com>
2022-12-13 21:00:49 +0700

    Add Profile/PostTableViewCell.swift

---
+++ Navigation/Profile/PostTableViewCell.swift
@@ -0,0 +1,129 @@
+//
+//  PostTableViewCell.swift
+//  Navigation
+//
+//  Created by Constantin on 13.12.2022.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    private lazy var spaceLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = .black
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    var author: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textColor = .black
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var descriptionText: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textColor = .systemGray
+        label.numberOfLines = 0
+        label.textAlignment = .justified
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var imageNews: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .black
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    var likes: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var views: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var stackViewVertical: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+//        stackView.contentMode = .scaleAspectFit
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    private lazy var stackViewHorizontal: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(stackViewVertical)
+        stackViewVertical.addArrangedSubview(spaceLabel)
+        stackViewVertical.addArrangedSubview(author)
+        stackViewVertical.addArrangedSubview(imageNews)
+        stackViewVertical.addArrangedSubview(descriptionText)
+        stackViewVertical.addArrangedSubview(stackViewHorizontal)
+        stackViewHorizontal.addArrangedSubview(likes)
+        stackViewHorizontal.addArrangedSubview(views)
+
+        installConstrains()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func installConstrains() {
+        NSLayoutConstraint.activate([
+            imageNews.widthAnchor.constraint(equalTo: stackViewVertical.widthAnchor),
+            imageNews.heightAnchor.constraint(equalTo: stackViewVertical.widthAnchor),
+
+            imageNews.leadingAnchor.constraint(equalTo: stackViewVertical.leadingAnchor),
+
+            author.leadingAnchor.constraint(equalTo: stackViewVertical.leadingAnchor, constant: 16),
+            author.trailingAnchor.constraint(equalTo: stackViewVertical.trailingAnchor, constant: -16),
+
+            descriptionText.trailingAnchor.constraint(equalTo: stackViewVertical.trailingAnchor, constant: -16),
+
+            stackViewHorizontal.trailingAnchor.constraint(equalTo: stackViewVertical.trailingAnchor, constant: -16),
+
+            stackViewVertical.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stackViewVertical.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            stackViewVertical.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackViewVertical.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        author.text = nil
+        imageNews.image = nil
+        descriptionText.text = nil
+        likes.text = nil
+        views.text = nil
+    }
+}
