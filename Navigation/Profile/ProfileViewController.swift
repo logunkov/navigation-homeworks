588dc8f04bc31b0b9fb94182197dff1c820de58a
logunkov <logunkovkn@gmail.com>
2022-12-23 10:13:28 +0700

    Add animation Profile/ProfileViewController.swift

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+final class ProfileViewController: UIViewController {
     
     private let authorArray = [
         "Екатерина Кулешова",
@@ -28,6 +28,11 @@ class ProfileViewController: UIViewController {
     private let imageArray = ["image1", "image2", "image3", "image4", "image5", "image6"]
     private let likesArray = [10, 20, 30, 40, 50 ,60]
     private let viewsArray = [100, 200, 300, 400, 500 ,600]
+
+    private var avatarWidthConstraint: NSLayoutConstraint!
+    private var avatarHeightConstraint: NSLayoutConstraint!
+    private var avatarXCenterConstraint: NSLayoutConstraint!
+    private var avatarYCenterConstraint: NSLayoutConstraint!
     
     private lazy var dataSource: [Post] = {
         var dataSource: [Post] = []
@@ -40,11 +45,8 @@ class ProfileViewController: UIViewController {
         }
         return dataSource
     }()
-    
-    private lazy var profileHeaderView: ProfileHeaderView = {
-        let view = ProfileHeaderView()
-        return view
-    }()
+
+    private let profileHeaderView = ProfileHeaderView()
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
@@ -55,31 +57,163 @@ class ProfileViewController: UIViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
+
+    private let avaterView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.alpha = 0
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private let imageView: UIImageView = {
+        let size = 150
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        imageView.image = UIImage(named: "popeye")
+        imageView.layer.borderWidth = 3
+        imageView.layer.backgroundColor = UIColor.purple.cgColor
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.isHidden = true
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = .black
+        button.alpha = 0
+        button.isHidden = true
+        button.setBackgroundImage(UIImage(systemName: "xmark.circle"), for: .normal)
+        button.addTarget(self, action: #selector(avatarClosed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private lazy var tapGestureRecognizerOneClick: UITapGestureRecognizer = {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.avatarOpened))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        return tapGestureRecognizer
+    }()
+
+    private lazy var tapGestureRecognizerTwoClicks: UITapGestureRecognizer = {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.touch))
+        tapGestureRecognizer.numberOfTapsRequired = 2
+        return tapGestureRecognizer
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Profile"
         self.navigationController?.navigationBar.isHidden = true
+        self.view.clipsToBounds = true
         self.view.backgroundColor = .lightGray
         self.view.addSubview(tableView)
+        self.view.addSubview(avaterView)
+        self.view.addSubview(imageView)
+        self.view.addSubview(closeButton)
         installConstrains()
-        
-        let recognizer = UITapGestureRecognizer(target: self, action: #selector(self.touch))
-        recognizer.numberOfTapsRequired = 2
-        recognizer.numberOfTouchesRequired = 1
-        view.addGestureRecognizer(recognizer)
+        installRecognizer()
     }
-    
+
     private func installConstrains() {
+        avatarWidthConstraint = imageView.widthAnchor.constraint(equalToConstant: 150)
+        avatarHeightConstraint = imageView.heightAnchor.constraint(equalToConstant: 150)
+        avatarXCenterConstraint = imageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 91)
+        avatarYCenterConstraint = imageView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 91 )
+
         NSLayoutConstraint.activate([
+            self.avatarWidthConstraint, self.avatarHeightConstraint,
+            self.avatarXCenterConstraint, self.avatarYCenterConstraint,
             tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+            avaterView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            avaterView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            avaterView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            avaterView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+            closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            closeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            closeButton.heightAnchor.constraint(equalToConstant: 44),
+            closeButton.widthAnchor.constraint(equalToConstant: 44),
         ])
     }
+
+    private func installRecognizer() {
+        profileHeaderView.imageView.addGestureRecognizer(tapGestureRecognizerOneClick)
+        view.addGestureRecognizer(tapGestureRecognizerTwoClicks)
+    }
+
     @objc func touch() {
         self.view.endEditing(true)
+    }
+
+    private func getInvisibility() {
+        self.profileHeaderView.imageView.isHidden.toggle()
+        self.closeButton.isHidden.toggle()
+        self.imageView.isHidden.toggle()
+    }
+
+    @objc func avatarOpened() {
+        let sizeWidth = CGFloat(floor((view.safeAreaLayoutGuide.layoutFrame.width)))
+        let sizeHeight = CGFloat(floor((view.safeAreaLayoutGuide.layoutFrame.height)))
+
+        avatarXCenterConstraint.constant = 91
+        avatarYCenterConstraint.constant = 91 - tableView.contentOffset.y
+        self.view.layoutIfNeeded()
+
+        getInvisibility()
+
+
+        if sizeWidth > sizeHeight {
+            avatarWidthConstraint.constant = sizeHeight
+            avatarHeightConstraint.constant = sizeHeight
+        } else {
+            avatarWidthConstraint.constant = sizeWidth
+            avatarHeightConstraint.constant = sizeWidth
+        }
+
+        avatarXCenterConstraint.constant = sizeWidth / 2
+        avatarYCenterConstraint.constant = sizeHeight / 2
+
+
+        UIView.animate(withDuration: 0.5) {
+            self.avaterView.alpha = 0.7
+            self.imageView.layer.cornerRadius = 0
+            self.view.layoutIfNeeded()
+        }
+
+        UIView.animate(withDuration: 0.3, delay: 0.5) {
+            self.closeButton.alpha = 1
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc func avatarClosed() {
+        UIView.animate(withDuration: 0.3) {
+            self.closeButton.alpha = 0
+            self.view.layoutIfNeeded()
+        }
+
+        avatarWidthConstraint.constant = 150
+        avatarHeightConstraint.constant = 150
+
+        avatarXCenterConstraint.constant = 91
+        avatarYCenterConstraint.constant = 91 - tableView.contentOffset.y
+
+
+        UIView.animate(withDuration: 0.5, delay: 0.3) {
+            self.avaterView.alpha = 0
+            self.imageView.layer.cornerRadius = 150 / 2
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            self.getInvisibility()
+        }
     }
 }
 
