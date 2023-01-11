2695de4d9ca118c3b7f4500a8a26cacc851a046a
logunkov <logunkovkn@gmail.com>
2023-01-11 21:09:11 +0700

    Fix PhotosViewController.swift

--- Navigation/Controller/PhotosViewController.swift
+++ Navigation/Controller/PhotosViewController.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 final class PhotosViewController: UIViewController {
+
+    private var imageWidthConstraint: NSLayoutConstraint!
+    private var imageHeightConstraint: NSLayoutConstraint!
+    private var imageXCenterConstraint: NSLayoutConstraint!
+    private var imageYCenterConstraint: NSLayoutConstraint!
+    private var imageSize:CGFloat!
+    private var imageSizeCenter:CGFloat!
+    private var numberRow: CGFloat = -1
+    private var numberColumn: CGFloat = -1
     
     private let layout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
@@ -28,13 +37,41 @@ final class PhotosViewController: UIViewController {
     }()
     
     private let dataSource: [UIImageView] = {
-        var imageArray: [UIImageView] = []
+        var images: [UIImageView] = []
         for index in 1...20 {
             let imageView = UIImageView()
             imageView.image = UIImage(named: "photo\(index)")
-            imageArray.append(imageView)
+            images.append(imageView)
         }
-        return imageArray
+        return images
+    }()
+
+    private let backView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.alpha = 0
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.isHidden = true
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = .black
+        button.alpha = 0
+        button.isHidden = true
+        button.setBackgroundImage(UIImage(systemName: "xmark.circle"), for: .normal)
+        button.addTarget(self, action: #selector(imageClosed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     override func viewDidLoad() {
@@ -44,20 +81,43 @@ final class PhotosViewController: UIViewController {
         self.view.backgroundColor = .lightGray
         
         view.addSubview(collectionView)
+        view.addSubview(backView)
+        view.addSubview(imageView)
+        view.addSubview(closeButton)
         installConstrains()
     }
     
     private func installConstrains() {
+        imageWidthConstraint = imageView.widthAnchor.constraint(equalToConstant: 119)
+        imageHeightConstraint = imageView.heightAnchor.constraint(equalToConstant: 119)
+        imageXCenterConstraint = imageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 91)
+        imageYCenterConstraint = imageView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 91 )
+
         NSLayoutConstraint.activate([
+            self.imageWidthConstraint, self.imageHeightConstraint,
+            self.imageXCenterConstraint, self.imageYCenterConstraint,
+
             collectionView.topAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.topAnchor),
             collectionView.bottomAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.bottomAnchor),
             collectionView.leadingAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.trailingAnchor)
+            collectionView.trailingAnchor.constraint(equalTo:  view.safeAreaLayoutGuide.trailingAnchor),
+
+            backView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            backView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            backView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            backView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+            closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            closeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            closeButton.heightAnchor.constraint(equalToConstant: 44),
+            closeButton.widthAnchor.constraint(equalToConstant: 44),
         ])
     }
     
     private func imageSize(for width: CGFloat, with spacing: CGFloat) -> CGSize {
         let width = floor((width - 4 * spacing) / 3)
+        imageSize = width
+        imageSizeCenter = 8 + width / 2
         return CGSize(width: width, height: width)
     }
 }
@@ -83,5 +143,62 @@ extension PhotosViewController: UICollectionViewDataSource, UICollectionViewDele
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let  spacing = (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.minimumInteritemSpacing
         return imageSize(for: collectionView.frame.width, with: spacing ?? 0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+        numberRow = CGFloat(indexPath.row / 3)
+        numberColumn = CGFloat(indexPath.row) - 3 * numberRow
+
+        imageView.image = UIImage(named: "photo\(indexPath.row + 1)")
+        let sizeWidth = CGFloat(floor((view.safeAreaLayoutGuide.layoutFrame.width)))
+        let sizeHeight = CGFloat(floor((view.safeAreaLayoutGuide.layoutFrame.height)))
+
+        imageXCenterConstraint.constant = imageSizeCenter + (imageSize + 8) * numberColumn
+        imageYCenterConstraint.constant = imageSizeCenter + (imageSize + 8) * numberRow - collectionView.contentOffset.y
+        self.view.layoutIfNeeded()
+
+        getInvisibility()
+
+        imageWidthConstraint.constant = sizeWidth
+        imageHeightConstraint.constant = sizeWidth
+
+        imageXCenterConstraint.constant = sizeWidth / 2
+        imageYCenterConstraint.constant = sizeHeight / 2
+
+        UIView.animate(withDuration: 0.5) {
+            self.backView.alpha = 0.7
+            self.view.layoutIfNeeded()
+        }
+
+        UIView.animate(withDuration: 0.3, delay: 0.5) {
+            self.closeButton.alpha = 1
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc func imageClosed() {
+        UIView.animate(withDuration: 0.3) {
+            self.closeButton.alpha = 0
+            self.view.layoutIfNeeded()
+        }
+
+        imageWidthConstraint.constant = imageSize
+        imageHeightConstraint.constant = imageSize
+
+        imageXCenterConstraint.constant = imageSizeCenter + (imageSize + 8) * numberColumn
+        imageYCenterConstraint.constant = imageSizeCenter + (imageSize + 8) * numberRow - collectionView.contentOffset.y
+
+        UIView.animate(withDuration: 0.5, delay: 0.3) {
+            self.backView.alpha = 0
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            self.getInvisibility()
+        }
+    }
+
+    private func getInvisibility() {
+        self.closeButton.isHidden.toggle()
+        self.imageView.isHidden.toggle()
     }
 }
